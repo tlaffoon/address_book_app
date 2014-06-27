55837c8a2ff59194e11d84be54e0c982e3b48ef8
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-06-27 15:28:52 -0500

    Refactored to implement sql queries and button functionality.

--- contacts.php
+++ contacts.php
@@ -1,5 +1,7 @@
 <?php
 
+// Add in-line editing for fields displayed in address book
+
 require('dbc.php');
 
 function getContacts($dbc, $contactsPerPage, $offset) {
@@ -10,13 +12,21 @@ function countContacts($dbc) {
 	return $dbc->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
 }
 
+function countContactAddresses($dbc, $contact_id) {
+	$query = "SELECT COUNT(*)
+				FROM contacts_addresses ca
+				JOIN contacts c on c.id = ca.contact_id
+				JOIN addresses a on a.id = ca.address_id
+				WHERE c.id = $contact_id;";
+    return $dbc->query($query)->fetchColumn();
+};
+
 function insertContact($dbc, $name, $phone) {
-	$query = 'INSERT INTO contacts (contact_name, phone) VALUES (:name, :phone);';
+	$query = 'INSERT INTO contacts (name, phone) VALUES (:name, :phone);';
 	$stmt = $dbc->prepare($query);
 	$stmt->bindValue(':name', $name, PDO::PARAM_STR);
 	$stmt->bindValue(':phone', $phone, PDO::PARAM_INT);
 	$stmt->execute();
-	return "<p>Inserted ID: " . $dbc->lastInsertId() . "</p>";
 }
 
 function removeContact($dbc, $id) {
@@ -34,7 +44,7 @@ $contacts = getContacts($dbc, $contactsPerPage, $offset);
 
 if (!empty($_POST)) {
 
-	$name = $_POST['name'];
+	$name = ucwords($_POST['name']);
 	$phone = $_POST['phone'];
 
 	try {
@@ -48,7 +58,7 @@ if (!empty($_POST)) {
 	} 
 
 	catch (Exception $e) {
-		echo "Error: " . $e->getMessage(); // alter to helptext beneath add contact form
+		$e->getMessage();
 	}
 }
 
@@ -62,7 +72,7 @@ if (!empty($_POST['remove'])) {
 <html>
 <head>
 	<title>Address Book: Contacts</title>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
 	<style type="text/css">
 	.spaced {
 			margin-bottom: 5px;
@@ -72,7 +82,7 @@ if (!empty($_POST['remove'])) {
 
 <body>
 
-	<div class="container col-md-8">
+	<div class="container">
 
 	<h1>Address Book: Contacts</h1>
 
@@ -88,18 +98,16 @@ if (!empty($_POST['remove'])) {
 
 		<table class="table table-striped">
 			<tr>
-				<th>Id</th>
 				<th>Name</th>
 				<th>Phone</th>
-				<th># of Address</th>
+				<th># of Addresses</th>
 				<th>Actions</th>
 			</tr>
 			<?php foreach ($contacts as $contact): ?>
 				<tr>
-					<td><?= $contact['id'] ?></td>
-					<td><?= $contact['contact_name'] ?></td>
+					<td><?= $contact['name'] ?></td>
 					<td><?= $contact['phone'] ?></td>
-					<td><? // # of addresses associated with this name?></td>
+					<td><?= countContactAddresses($dbc, $contact['id']) ?></td>
 					<td>
 						<a class="btn btn-small btn-default" href="contact_addresses.php?contact_id=<?= $contact['id'] ?>">View</a>
 						<button class="btn btn-small btn-danger btn-remove" data-contact="<?= $contact['id'] ?>">Remove</button>
@@ -107,8 +115,8 @@ if (!empty($_POST['remove'])) {
 				</tr>
 			<?php endforeach ?>
 		</table>
-
-		<form class="form-inline" role="form" action="contacts.php" method="POST">
+		<h3>Add a Contact: </h3>
+		<form class="form-inline" role="form" action="" method="POST">
 			<div class="form-group">
 				<label class="sr-only" for="name">Name</label>
 				<input type="text" name="name" id="name" class="form-control" placeholder="Name">
@@ -119,25 +127,24 @@ if (!empty($_POST['remove'])) {
 			</div>
 			<button type="submit" class="btn btn-default btn-success">Add Contact</button> 
 		</form>
-	</div> <? // end main container ?>
+	</div>
 
-
-<form id="remove-form" action="contacts.php" method="post">
+<form id="remove-form" action="" method="post">
 	<input id="remove-id" type="hidden" name="remove" value="">
 </form>
 
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
- <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script>
+	console.log('Document Loaded.');
 
-$('.btn-remove').click(function () {
-	var contactId = $(this).data('contact');
-	if (confirm('Are you sure you want to remove contact ' + contactId + '?')) {
+	$('.btn-remove').click(function () {
+		var contactId = $(this).data('contact');
+		if (confirm('Are you sure you want to remove contact ' + contactId + '?')) {
 		$('#remove-id').val(contactId);
 		$('#remove-form').submit();
-	}
-});
-
+		}
+	});
 </script>
 
 </body>
